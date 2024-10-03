e136609d4b21606129a4bc715a9265886722ed98
Sofia <161875100+Sofiafs@users.noreply.github.com>
2024-10-03 00:21:04 -0300

    Vigesimo segundo commit

--- BD/safehome.sql
+++ BD/safehome.sql
@@ -5,16 +5,16 @@ USE cadastro_login;
 -- Tabela de Cadastro 
 CREATE TABLE IF NOT EXISTS Cadastro (
     id_usuario INT AUTO_INCREMENT,     
-    nome VARCHAR(255) NOT NULL,        
-    email VARCHAR(255) NOT NULL UNIQUE, 
-    senha VARCHAR(255) NOT NULL,      
+    nome VARCHAR(100) NOT NULL,        
+    email VARCHAR(100) NOT NULL UNIQUE, 
+    senha VARCHAR(40) NOT NULL,      
     PRIMARY KEY (id_usuario) 
              
 -- Tabela de Login 
 CREATE TABLE IF NOT EXISTS Login (
     id_login INT AUTO_INCREMENT,      
-    email VARCHAR(255) NOT NULL,       
-    senha VARCHAR(255) NOT NULL,      
+    email VARCHAR(100) NOT NULL,       
+    senha VARCHAR(40) NOT NULL,      
     login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY (id_login),            
     FOREIGN KEY (email) REFERENCES Cadastro(email) 
